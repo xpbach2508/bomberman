a6a84524a7a39cf30158d69f02076fcbd4bcccc2
Phạm Minh Hiếu 21020319 <21020319@vnu.edu.vn>
2022-10-31 21:51:08 +0700

    push uml diagram.

--- src/bomberman/entities/Enemies/Balloom.java
+++ src/bomberman/entities/Enemies/Balloom.java
@@ -34,7 +34,7 @@ public class Balloom extends Enemies {
         if (direct == 1) {
             if (canMove(x + 4, y)) {
                 moveX = 1;
-                if (x - preX > 24) if (canMove(x, y - 4) || canMove(x, y + 4)) {
+                if (x - preX > 24) if (canMove(x, y - 8) || canMove(x, y + 8)) {
                     direct = random(new int[]{1, 0, 2});
                     setChangedCoordinates(x, y);
                 }
@@ -58,7 +58,7 @@ public class Balloom extends Enemies {
         if (direct == 3) {
             if (canMove(x - 4, y)) {
                 moveX = -1;
-                if (preX - x > 24) if (canMove(x+10, y - 4) || canMove(x+10, y + 4)) {
+                if (preX - x > 24) if (canMove(x, y - 8) || canMove(x, y + 8)) {
                     direct = random(new int[]{3, 0, 2});
                     setChangedCoordinates(x, y);
                 }
